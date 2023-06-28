12e914175e604ab91a07cb6598b94580bcfe3a5a
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2023-06-28 18:23:11 +0900

    생활코딩 8/14

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,28 @@
 from django.http import HttpResponse
+import random
 from django.shortcuts import render
+
+topics = [
+    {'id' : 1, 'title' : 'routing', 'body' : 'Routing is ...'},
+    {'id' : 2, 'title' : 'view', 'body' : 'View is ...'},
+    {'id' : 3, 'title' : 'Model', 'body' : 'Model is ...'}
+]
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Welcom!</h1>')
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+
+    return HttpResponse(f'''
+    <h1>Django</h1>
+    <ol>
+        {ol}
+    </ol>
+    <h2>Welcome</h2>
+    Hello, Django
+    ''')
 
 def create(request):
     return HttpResponse('<h1>Create!</h1>')
